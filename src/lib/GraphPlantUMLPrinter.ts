0716e9e5809f40f65f2bab860f116e9ee2162b98
rvarjabedian <rvarjabedian@wayfair.com>
2022-09-28 12:26:32 -0400

    redistribute costs for better looking graphs

--- src/lib/GraphPlantUMLPrinter.ts
+++ src/lib/GraphPlantUMLPrinter.ts
@@ -1,13 +1,21 @@
-import { IGraph } from "./interfaces/IGraph";
-import { Node } from "./typeDefs";
-import { EOL } from "os";
+import {IGraph} from "./interfaces/IGraph";
+import {Node} from "./typeDefs";
+import {EOL} from "os";
 
 export class GraphPlantUMLPrinter {
     public static generateContents(start: Node, graph: IGraph<unknown>, directional: boolean, addLength = true) {
-        // note: minlen / 2 = helps keep the graph smaller, remove /2 if you want it to be 100% sized relative to the cost
+        // redistribute the relative values of the costs to a linear one for nicer looking graphs
+        const costsHash: { [key: number]: string } = [...new Set(graph.getAllPaths(directional).map(({cost}) => cost))]
+            .sort((a, b) => a - b)
+            .reduce((acc, cost, currentIndex) => {
+                return {
+                    ...acc,
+                    [cost]: (currentIndex + 1).toString()
+                }
+            }, {});
         const steps = graph.getAllPaths(directional).map((edge) => {
             return `${edge.from} -> ${edge.to} [label = "${edge.cost}"${
-                addLength ? ", minlen=" + Math.floor(edge.cost / 2).toString() : ""
+                addLength ? ", minlen=" + costsHash[edge.cost] : ""
             }];`;
         });
         return `@startdot
